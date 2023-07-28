d6bfa0616c4f00406f33e94a348fa7c17633391a
jinxin <jinxincandor@163.com>
2023-07-28 18:24:40 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     // 这意味着每当数据通过控制器Controller进入应用程序时，都会自动应用ValidationPipe进行验证
     app.useGlobalPipes(new ValidationPipe()); // 全局启用 ValidationPipe
 
+    app.enableCors();
     await app.listen(3000);
 }
 bootstrap();
